72bada7ed49abd6d65721cce59c6e45fa61e1eaa
Kai Lam <kailamnyc@gmail.com>
2023-09-28 22:04:27 -0400

    working on form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 
 import { Button } from "@/components/ui/button";
@@ -9,6 +10,15 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
@@ -18,45 +28,157 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Icons } from "@/components/icons";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
 
 export default function Home() {
+	const form = useForm();
+	function onSubmit(values) {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		console.log(values);
+	}
+
 	return (
-		<div>
-			<Card className='w-[350px]'>
-				<CardHeader>
-					<CardTitle>Hello!</CardTitle>
-					<CardDescription>
-						Deploy your new project in one-click.
-					</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<form>
-						<div className='grid w-full items-center gap-4'>
-							<div className='flex flex-col space-y-1.5'>
-								<Label htmlFor='name'>Name</Label>
-								<Input id='name' placeholder='Name of your project' />
+		<div className='flex justify-center items-center h-screen '>
+			<Card className='max-w-full spa'>
+				<div className='flex items-center '>
+					<div>
+						<CardHeader>
+							<Image src='/logo4.svg' alt='logo' height={50} width={160} />
+							<CardTitle>Hello!</CardTitle>
+
+							<Separator />
+
+							<CardDescription>Create Your Account</CardDescription>
+							{/* <div className='relative'>
+							<div className='absolute inset-0 flex items-center'>
+								<span className='w-full border-t' />
 							</div>
-							<div className='flex flex-col space-y-1.5'>
-								<Label htmlFor='framework'>Framework</Label>
-								<Select>
-									<SelectTrigger id='framework'>
-										<SelectValue placeholder='Select' />
-									</SelectTrigger>
-									<SelectContent position='popper'>
-										<SelectItem value='next'>Next.js</SelectItem>
-										<SelectItem value='sveltekit'>SvelteKit</SelectItem>
-										<SelectItem value='astro'>Astro</SelectItem>
-										<SelectItem value='nuxt'>Nuxt.js</SelectItem>
-									</SelectContent>
-								</Select>
+							<div className='relative flex justify-center text-xs uppercase'>
+								<span className='bg-background px-2 text-muted-foreground'>
+									Or continue with
+								</span>
 							</div>
-						</div>
-					</form>
-				</CardContent>
-				<CardFooter className='flex justify-between'>
-					{/* <Button variant='outline'>Cancel</Button> */}
-					<Button>SIGN UP</Button>
-				</CardFooter>
+						</div> */}
+							{/* <Separator /> */}
+						</CardHeader>
+						<CardContent>
+							<Form {...form}>
+								<form
+									onSubmit={form.handleSubmit(onSubmit)}
+									className='space-y-4'
+								>
+									<FormField
+										control={form.control}
+										name='firstName'
+										render={({ field }) => (
+											<FormItem>
+												{/* <FormLabel>Username</FormLabel> */}
+												<FormControl>
+													<Input placeholder='First Name' {...field} />
+												</FormControl>
+												{/* <FormDescription>
+												This is your public display name.
+											</FormDescription> */}
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name='lastName'
+										render={({ field }) => (
+											<FormItem>
+												{/* <FormLabel>Username</FormLabel> */}
+												<FormControl>
+													<Input placeholder='Last Name' {...field} />
+												</FormControl>
+												{/* <FormDescription>
+												This is your public display name.
+											</FormDescription> */}
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name='emailId'
+										render={({ field }) => (
+											<FormItem>
+												{/* <FormLabel>Username</FormLabel> */}
+												<FormControl>
+													<Input placeholder='Email-id' {...field} />
+												</FormControl>
+												{/* <FormDescription>
+												This is your public display name.
+											</FormDescription> */}
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name='password'
+										render={({ field }) => (
+											<FormItem>
+												{/* <FormLabel>Username</FormLabel> */}
+												<FormControl>
+													<Input placeholder='Password' {...field} />
+												</FormControl>
+												{/* <FormDescription>
+												This is your public display name.
+											</FormDescription> */}
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name='confirmPassword'
+										render={({ field }) => (
+											<FormItem>
+												{/* <FormLabel>Username</FormLabel> */}
+												<FormControl>
+													<Input placeholder='Confirm Password' {...field} />
+												</FormControl>
+												{/* <FormDescription>
+												This is your public display name.
+											</FormDescription> */}
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<Button type='submit'>SIGN UP</Button>
+								</form>
+							</Form>
+						</CardContent>
+						<CardFooter className='flex flex-col'>
+							<p className='text-sm mb-2'>OR</p>
+							<div className='grid grid-cols-2 gap-4 mb-2'>
+								<Button variant='outline'>
+									<Icons.google className='mr-2 h-4 w-4' />
+									Google
+								</Button>
+								<Button variant='outline'>
+									<Icons.gitHub className='mr-2 h-4 w-4' />
+									Github
+								</Button>
+							</div>
+							<p className='text-sm mb-2'>
+								Already have an Account? <Link href='/login'>LOGIN</Link>
+							</p>
+						</CardFooter>
+					</div>
+					<Separator orientation='vertical' />
+					<Image src='/bro.svg' alt='bro' height={326} width={500} />
+				</div>
 			</Card>
 		</div>
 	);
