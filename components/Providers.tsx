8b0899b2f22486dd474d85bc987f2964824dc4cd
Kai Lam <kailamnyc@gmail.com>
2023-10-02 00:13:42 -0400

    deployment session stuff

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,7 +2,7 @@
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 
-const Providers = ({ children, session }) => {
+const Providers = ({ children, session, ...props }) => {
 	return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 export default Providers;
